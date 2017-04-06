b8f9d33362c3f77248487cad483d480b347cb56d
Christian Kniep <christian@qnib.org>
2017-04-06 15:53:33 +0200

    remove comment

--- main.go
+++ main.go
@@ -63,24 +63,6 @@ func main() {
 			os.Exit(1)
 		}
 		return
-		/*
-		if [ ${STATUS} = "green" ];then
-  			echo "Cluster is green, all fine..."
-  			exit 0
-		elif [ ${STATUS} = "red" ];then
-		  echoerr "Cluster is red!... "
-		  exit 1
-		elif [ ! -f /tmp/es_initialised ] && [ ${STATUS} = "yellow" ];then
-		  echoerr "Node has not initialised, and cluster is yellow -> considered an error..."
-		  exit 1
-		elif [ ${STATUS} = "yellow" ];then
-		  echo "Cluster is yellow but we are already over the initialisation phase and assume someone else was updated."
-		  exit 0
-		else
-		  echoerr "Cluster is neither green,yello or red, but '${STATUS}'"
-		  exit 1
-		fi
-		 */
 	}
 
 
